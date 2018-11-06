6e87ebf87fc3cc9f1ea5444ddaf6b59211f8173c
Trung-Hieu Ho <piouscity@outlook.com>
2018-11-06 22:50:25 +0700

    Drawing a simple rectangle.

--- Testing/Form1.cs
+++ Testing/Form1.cs
@@ -16,5 +16,22 @@ namespace Testing
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphic1 = Graphics.FromImage(pictureBox1.Image);
+            Graphics graphic2 = Graphics.FromImage(pictureBox2.Image);
+            Pen blackpen = new Pen(Color.Black, 3);
+            Rectangle rect = new Rectangle(10, 10, 50, 60);
+
+            graphic1.DrawRectangle(blackpen, rect);
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+        }
     }
 }
